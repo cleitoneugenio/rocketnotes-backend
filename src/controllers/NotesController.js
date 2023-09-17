da59d7ba5fd5b8f1057d043bc36c654de9723e42
cleitoneugenio <cleitoneugenio87@gmail.com>
2023-09-16 23:46:58 -0300

    test

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -64,6 +64,8 @@ class NotesController {
 
         let notes;
 
+        console.log("test");
+
         if(tags) {
             const filterTags = tags.split(',').map(tag => tag.trim());
             
